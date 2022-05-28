82ffeae41549db2e340921e52f2257b9a2aa123f
ElmiraZairbekova <loveni-shop@yandex.ru>
2022-05-29 00:23:25 +0300

    сделать submit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Находим в DOM попап и его элементы, присваиваем константам
-
 let popup = document.querySelector('.popup');
 let btnEditProfile = document.querySelector('.profile__edit-button');
 let btnClosePopup = document.querySelector('.popup__close');
@@ -9,9 +7,6 @@ let profileDescription = document.querySelector('.profile__description');
 let profileNameInput = document.querySelector('.popup__input-name');
 let profileDescriptionInput = document.querySelector('.popup__input-description');
 
-// добавляем модификатор, чтобы попап открывался 
-// Подставляем в поля текущие значения со страницы, убирая пробелы 
-
 function popupOpen() {
     console.log("open");
     popup.classList.add('popup_active');
@@ -19,14 +14,9 @@ function popupOpen() {
     profileDescriptionInput.value = profileDescription.textContent.trim();
 }
 
-// чтобы закрыть попап, удаляем модификатор 
-
 function popupClose() {
     popup.classList.remove('popup_active');
 }
-
-// добавляем обработчик, не забыв про отключение дефолта
-// по сабмиту возвращаем значения, введенные в форме 
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -36,8 +26,6 @@ function formSubmitHandler (evt) {
     profileDescription.textContent = profileDescriptionInput.value;
     popupClose();
 }
-
-// добавляем вызов функций через подписку на click и submit  
 
 btnEditProfile.addEventListener('click', popupOpen);
 btnClosePopup.addEventListener('click', popupClose);
